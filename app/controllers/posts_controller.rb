8e505415166a074199135e4f346d4ac377d16175
abhishek <ab1hi2shek@gmail.com>
2017-02-13 05:10:01 +0530

    added pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
   		if params[:search]
-    		@posts = Post.search(params[:search]).order("created_at DESC")
+    		@posts = Post.search(params[:search]).order("created_at DESC").paginate(page: params[:page], per_page: 4)
   		else
-    		@posts = Post.all.order('created_at DESC')
+    		@posts = Post.all.order('created_at DESC').paginate(page: params[:page], per_page: 4)
   		end
 	end
 
